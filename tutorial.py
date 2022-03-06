fad584bd7115abdbadfbb3573864351db9d95ac8
akr16 <100000413+akr16@users.noreply.github.com>
2022-03-06 14:12:58 +0000

    v1.0 stable

--- tutorial.py
+++ tutorial.py
@@ -2,10 +2,11 @@ import curses
 from curses import wrapper
 import enum
 from multiprocessing.reduction import steal_handle
+from random import random
 from sys import stderr
 from textwrap import wrap
 import time
-
+import random
 
 
 
@@ -30,10 +31,13 @@ def display_text(stdscr, target_text, current_text, wpm=0):
 
         stdscr.addstr(0, i, char, color)
 
-
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
 
 def wpm_test(stdscr):
-    target_text = "hellow world this is some text for this app"
+    target_text = load_text()
     current_text = []
     wpm = 0
     start_time = time.time()
@@ -42,9 +46,16 @@ def wpm_test(stdscr):
     while True:
         time_elapsed = max(time.time() - start_time, 1)
         wpm = (len(current_text) / (time_elapsed / 60) / 5)
+        
         stdscr.clear()
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
+
+
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
         
         try:
             key = stdscr.getkey()
@@ -68,6 +79,13 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+   
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "you completed the test! Press any key to continue...")
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
+
 
 wrapper(main)
